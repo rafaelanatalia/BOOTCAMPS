01a439c1bca8429e9c8e595b80f7d019aaea6852
Rafaela Natalia <rafaela.natalia1998@hotmail.com>
2021-10-18 16:43:00 -0300

    desafio basico de soma simples js

---
+++ Desafios Basico em Javascript/soma simples.js
@@ -0,0 +1,7 @@
+/*Imprima a variável SOMA com todas as letras maiúsculas, inserindo um espaço em branco antes e depois do símbolo de igualdade, seguido pelo valor correspondente à soma de A e B.*/
+
+
+let A = parseInt(gets());
+let B = parseInt(gets());
+let SOMA = A+B ;
+console.log("SOMA = "+ (A+B));
